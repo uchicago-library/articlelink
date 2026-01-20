c0c9f081387feb61fecae19a113e322ac97eaeb2
Matt Teichman <teichman@uchicago.edu>
2026-01-20 11:11:57 -0600

    cgi module

--- lib/cgi.ml
+++ lib/cgi.ml
@@ -1,11 +1,55 @@
-let list_to_option = function | [] -> None
-                              | x :: _ -> Some x
-
-let lookup_opt key qs =
-  Option.bind (List.assoc_opt key qs) list_to_option
-
 let lookup key qs =
-  match lookup_opt key qs with
-  | Some v -> Ok v
-  | None -> Error (key ^ " not found in query string")
+  match List.assoc_opt key qs with
+  | Some (v :: _) -> Ok v
+  | _ -> Error (key ^ " not found in query string")
 
+module Input = struct
+  type mode =
+    | JSON of string
+    | JSONP of string * string
+    | XMLDebug of string
+end
+
+let parse_qs qs =
+  let query = Uri.query_of_encoded qs in
+  let lookups =
+    lookup "openurl" query ,
+    lookup "callback" query ,
+    lookup "xml" query
+  in
+  match lookups with
+  | Ok openurl, Ok callback, Error _ ->
+     Ok (Input.JSONP (openurl, callback))
+  | Ok openurl, Error _, Error _
+    | Ok openurl, Error _, Ok "false" ->
+     Ok (Input.JSON openurl)
+  | Ok openurl, _, Ok "true" ->
+     Ok (Input.XMLDebug openurl)
+  | Ok _, _, Ok other ->
+     let msg = "xml parameter was "
+               ^ other
+               ^ "; should be true or false"
+     in
+     Error msg
+  | Error e, _, _ ->
+     Error e
+
+module Output = struct
+  type mode =
+    | JSON of (string * string) list
+    | JSONP of (string * string) list * string
+    | XMLDebug of string
+end
+
+let compute =
+  let open Etude.Result.Make (String) in
+  function
+  | Input.JSON openurl ->
+     let+ hits = Sfx.openurl_to_article openurl
+     in Output.JSON hits
+  | Input.JSONP (openurl, callback) ->
+     let+ hits = Sfx.openurl_to_article openurl
+     in Output.JSONP (hits, callback)
+  | Input.XMLDebug openurl ->
+     let+ xml_string = Sfx.openurl_to_xml openurl
+     in Output.XMLDebug xml_string
