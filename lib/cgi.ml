a32f9781fcd8cf5f1b7f4af75f9925f2a2a32ce5
Matt Teichman <teichman@uchicago.edu>
2026-01-19 21:22:15 -0600

    result lookup

--- lib/cgi.ml
+++ lib/cgi.ml
@@ -1,5 +1,11 @@
 let list_to_option = function | [] -> None
                               | x :: _ -> Some x
 
-let lookup key qs =
+let lookup_opt key qs =
   Option.bind (List.assoc_opt key qs) list_to_option
+
+let lookup key qs =
+  match lookup_opt key qs with
+  | Some v -> Ok v
+  | None -> Error (key ^ " not found in query string")
+
