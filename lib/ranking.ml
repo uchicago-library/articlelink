afcc2afdf80125ca912f42966b5be14b7275585f
Matt Teichman <teichman@uchicago.edu>
2026-02-05 15:34:35 -0600

    sorting isn't working; in the middle of debugging it

--- lib/ranking.ml
+++ lib/ranking.ml
@@ -1,3 +1,5 @@
+let prios_file = "lib/prios/prios"
+
 let list_to_tup lst =
   let open Etude.Option in
   let+ provider = List.nth_opt lst 0
@@ -7,6 +9,15 @@ let list_to_tup lst =
 let csv_list_to_tups lst =
   Etude.Option.traverse list_to_tup (Prelude.drop 1 lst)
 
+let csv_load_opt filepath =
+  match Csv.load filepath with
+  | exception _ -> None
+  | legit -> Some legit
+
+let sort_tups tups =
+  let comparison (_, i) (_, j) = Int.compare i j in
+  List.stable_sort comparison tups
+
 let cleanup str =
   let each_char = function
     | '_' -> ' '
@@ -14,13 +25,30 @@ let cleanup str =
   in
   String.map each_char str
 
-let sort_tups tups =
-  let comparison (_, i) (_, j) = Int.compare i j in
-  List.stable_sort comparison tups
-
 let filepath_to_ranking filepath =
   let open Etude.Option in
-  let csv = Csv.load filepath in
-  let+ tups = csv_list_to_tups csv in
+  let* csv = csv_load_opt filepath in
+  let* tups = csv_list_to_tups csv in
   let cleanup_project (x, _) = cleanup x in
-  tups |> sort_tups |> List.map cleanup_project
+  pure (tups |> sort_tups |> List.map cleanup_project)
+
+let compy ranking (x, _) (y, _) =
+  Etude.Sort.list_to_ranking ranking (cleanup x) (cleanup y)
+
+let rec get_idx n key = function
+  | [] -> n
+  | x :: xs -> if key = x then n else get_idx (n + 1) key xs
+
+let sort_hits hits =
+  let ranking = filepath_to_ranking prios_file in
+  match ranking with
+  | None -> hits
+  | Some r ->
+    let comp x y =
+      Printf.printf "%s: %i\n%s: %i\n\n" (fst x)
+        (get_idx 0 (cleanup (fst x)) r)
+        (fst y)
+        (get_idx 0 (cleanup (fst y)) r) ;
+      compy r x y
+    in
+    List.stable_sort comp hits
